58716d7b66605d8d387bd4b4a7d6ff07f50d9304
Melissa Paradiso <meltheeclipsequeen@gmail.com>
2018-12-04 15:27:18 -0600

    Fixed sessioncart

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -24,6 +24,8 @@ namespace SportsStore
                 Configuration["Data:SportsStoreProducts:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
             //services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddMvc();
             services.AddMemoryCache();
             services.AddSession();
